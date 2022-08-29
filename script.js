6aa61eeebc5607a110d51d312ec921d6b58525b8
Ojingwa Daniel <79636701+ojingwaDaniel@users.noreply.github.com>
2022-08-29 11:27:38 +0100

    using functions

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ let playingCard = {
     shuffle() {
         const {playTable} = this
         for (let i = playTable.length -1; i > 0; i--) {
-            let j = Math.floor(Math.random()*(i + 1));
+            let j = Math.floor(Math.random()*(i + 1))
             [playTable[i], playTable[j]] = [playTable[j],playTable[i]]
         }
         
@@ -42,4 +42,24 @@ let playingCard = {
     }
 
      
+}
+function initiate(){
+   let  values = ['2','3','4','5','6','7','8','9','10','K','Q','A','J']
+   let suits = ['hearts','diamond','spades','clubs']
+   let deck = []
+   for (let value of values) {
+    for(let suit of suits){
+        deck.push({
+            value,
+            suit
+        })
+    }
+    
+   }
+   return deck
+}
+function PlayCard(){
+    let card = []
+    card.push(initiate().pop())
+    return card
 }
